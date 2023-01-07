9da0d7d41111f25fc1ffd5266d563e24cb0ff329
Lew Bloch <lewbloch@gmail.com>
2023-01-07 14:51:59 -0800

    Start Python version.

---
+++ src/main/python/com/lewscanon/triangles/naive_triangles.py
@@ -0,0 +1,39 @@
+# Copyright © 2023, Lewis S. Bloch. All rights reserved.
+
+from enum import Enum
+
+
+class TriangleType(Enum):
+    SCALENE = 'SCALENE'
+    ISOSCELES = 'ISOSCELES'
+    EQUILATERAL = 'EQUILATERAL'
+
+
+def evaluate_triangle_type(side1, side2, side3):
+    if side1 == side2 or side1 == side3:
+        if side2 == side3:
+            return TriangleType.EQUILATERAL
+        else:
+            return TriangleType.ISOSCELES
+    elif side2 == side3:
+        return TriangleType.ISOSCELES
+    else:
+        return TriangleType.SCALENE
+
+
+def obtain_sides():
+    test_data = [
+        [1.0, 1.0, 1.0],
+        [1.0, 1.5, 1.5],
+        [1.0, 1.5, 2.0],
+        [1.0, 1.0, 2.0],
+        [0.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0],
+    ]
+    return test_data
+
+
+for sides in obtain_sides():
+    triangle_type = evaluate_triangle_type(sides[0], sides[1], sides[2])
+    message = f"The triangle with sides {sides} is {str(triangle_type)}"
+    print(message)
